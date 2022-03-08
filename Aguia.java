62bd785aef231ba29a9962e06ac7b003e6e9e540
fjydrtm <enzo_tsutsumi@hotmail.com>
2022-03-08 19:19:54 +0000

    Exemplo_classe_abstrata

---
+++ Aguia.java
@@ -0,0 +1,19 @@
+public class Aguia extends Ave {
+
+    @Override
+    public String nadar() {
+        // TODO Auto-generated method stub
+        return "Nada mal";
+    }
+    @Override
+    public String voar() {
+        // TODO Auto-generated method stub
+        return "Voa bem!";
+    }
+
+    public void mensge(){
+        System.out.println("");
+    }
+
+    
+}
